2a2e41046c1e0036f2d02ee91ccb7fe4973d125a
MichaelDot <Michael.woodrum@gmail.com>
2023-12-29 07:19:48 -0500

    added some new function

---
+++ _posts/2023-12-29-My-2024-Coding-Goals-Focusing-on-AI.md
@@ -0,0 +1,13 @@
+---
+date: 2023-12-29
+header:
+  teaser: /img/coding20242.png
+  overlay_image: /img/coding20242.png
+  overlay_filter: 0.8
+toc: true
+toc_label: "Contents"
+---
+
+# My 2024 Coding Goals Focusing on AI
+
+Lorem Ipsum
--- new_post.py
+++ new_post.py
@@ -1,28 +1,55 @@
 from datetime import datetime
+import os
+import subprocess
+import platform
 
 # Current date in the desired format
 current_date = datetime.now().strftime("%Y-%m-%d")
 
-# Template for the markdown file content
+# Function to get the post name from the user
+def get_post_name():
+    post_name = input("Enter the name of the post: ")
+    file_post_name = post_name.replace(" ", "-")
+    return post_name, file_post_name
+
+# Function to open the /img/ directory in file explorer
+def open_image_directory():
+    img_path = os.path.abspath("./img/")
+    if platform.system() == "Windows":
+        os.startfile(img_path)
+    elif platform.system() == "Darwin":  # macOS
+        subprocess.Popen(["open", img_path])
+    else:  # Linux and other OS
+        subprocess.Popen(["xdg-open", img_path])
+
+    return input("Enter the path of the chosen image or press Enter to skip: ")
+
+# Get the post name and formatted name
+post_name, file_post_name = get_post_name()
+
+# Open image directory and get image path from user
+image_path = open_image_directory()
+
+# Use the chosen image or the default placeholder
+image_path = image_path or "/img/replace.jpg"
+
+# Template for the markdown file content with dynamic header and image
 markdown_template = f"""---
 date: {current_date}
 header:
-  teaser: /img/replace.jpg
-  overlay_image: /img/replace.jpg
+  teaser: {image_path}
+  overlay_image: {image_path}
   overlay_filter: 0.8
 toc: true
 toc_label: "Contents"
---- 
+---
 
-# Header
+# {post_name}
 
 Lorem Ipsum"""
 
 # File name for the new markdown file
-file_name = f"{current_date}-newpost.md"
-
-
-import os
+file_name = f"{current_date}-{file_post_name}.md"
 
 # Function to create and write the markdown file
 def create_markdown_file(file_name, content):
@@ -31,7 +58,6 @@ def create_markdown_file(file_name, content):
     with open(file_path, "w") as file:
         file.write(content)
     return f"File '{file_path}' created successfully."
-
 
 # Create the markdown file
 create_markdown_file(file_name, markdown_template)
